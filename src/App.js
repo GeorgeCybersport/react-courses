4f3a3dbf6db47552a5c891f59598fcd26525c9a0
GeorgeCybersport <solokhin-eg@yandex.ru>
2022-04-07 18:10:35 +0300

    lesson2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { Container } from '@mui/material';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Chat } from './components/Chat';
+import { Header } from './components/Header/Header';
 import { Message } from './components/Message';
+import { ROUTES, SINGLE_CHAT } from './constants/routes';
+import { Chats } from './pages/Chats';
+import { MainPage } from './pages/MainPage';
+import { Messages } from './pages/Messages';
 
 function App() {
-  const message = {
-    author: 'Peter Pan',
-    text: 'I wish to see you!',
-  };
   return (
     <Container className="App">
-      <Message message={message} />
+      <Header />
+      <Routes>
+        <Route path={ROUTES.MESSAGES.path} element={<Messages />} exact />
+        <Route path={ROUTES.MAIN.path} element={<MainPage />} exact />
+        <Route path={ROUTES.CHATS.path} element={<Chats />} exact />
+        <Route path={SINGLE_CHAT.path} element={<Chat />} exact />
+        <Route path="*" element={<Navigate to={ROUTES.MAIN.path} replace />} />
+      </Routes>
     </Container>
   );
 }
